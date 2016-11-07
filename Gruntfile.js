7dd883fb96a11fa1913dbc80bb4bcb721419148f
Barbara Banach <b_code@MacBook-Air-Barbara.local>
2016-11-07 13:22:47 +0100

    added all files

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'css/main.css': 'sass/main.sass'
+            }
+        }
+    },
+
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{png, jpg,jpeg, gif}'],
+                dest: 'images/build/'
+            }]
+        }
+    },
+
+
+
+    watch: {
+        scripts: {
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        }
+    },
+    
+    browserSync: {
+        files: {
+          src : [
+              'assets/css/*.css',
+              'assets/img/*',
+              'assets/js/*.js',
+              '**/*.html'
+           ],
+        },
+        options: {
+            watchTask: true
+        }
+    },
+
+
+  });
+
+  //Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+
+  //Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
+
+};
